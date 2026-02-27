2ee5d73da9af1844d0008286e82b641f3cedaf92
Yusuf Yonturk <yusufyonturk.kisisel@gmail.com>
2026-02-28 00:06:39 +0300

    initial commit

---
+++ web-lab-hello/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama </h1>
+      <h2>LAB -1</h2>
+      <p>Ad Soyad: Yusuf Yontürk</p>
+      <p>Ogrenci No: 240541165</p>
+      </div>
+
+  )
+}
+
+export default App
